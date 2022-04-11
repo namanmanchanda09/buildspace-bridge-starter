aef1eb3c4ab4e2a96c2fcf4a4891548b87be60b5
Naman Manchanda <naman.mohan09@outlook.com>
2022-04-11 11:49:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <center>
+        <h2>Hey homies!</h2>
+        <h3>Welcome to the 1st Buildspace mini project.</h3>
+        <span style={{color:'blue'}}><h1>We gonna be shipping an eth-polygon bridge :).</h1></span>
+      </center>
+    </div>
+  );
+}
+
+export default App;
